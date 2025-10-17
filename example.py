f87ef16065471c770657903296dcc600d1f5d1f6
pirorintyo <watahiro0716@icloud.com>
2025-10-17 20:34:57 +0900

    python_practice

---
+++ example.py
@@ -0,0 +1,27 @@
+#1
+print("hello world")
+
+#2
+def greet():
+    print("こんにちは")
+
+greet()
+
+#3
+def print_name(name):
+    print(f"私の名前は{name}です")
+
+print_name("name")
+
+#4
+def get_greet():
+    return "おはようございます"
+
+print(get_greet())
+
+#5
+def add(a,b):
+    return a+b
+
+print(add(int(input()),int(input())))
+#fin
